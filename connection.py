bf394c821a52f63f3bd305a1d21f69de4ec2fcc9
BaronFonMoc <sem.tima@yandex.ru>
2022-12-19 14:53:53 +0300

    Added basic flask app

---
+++ connection.py
@@ -0,0 +1,16 @@
+import MySQLdb
+
+conn = MySQLdb.connect(
+      host="rc1b-8xtt6v6j6v24qm6v.mdb.yandexcloud.net",
+      port=3306,
+      db="db1",
+      user="user1",
+      passwd="<user password>",
+      ssl={'ca': '~/.mysql/root.crt'})
+
+cur = conn.cursor()
+cur.execute('SELECT version()')
+
+print(cur.fetchone()[0])
+
+conn.close()
